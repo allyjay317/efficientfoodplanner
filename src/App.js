73b80f1431530f74f33663092c75c6a51cea26a8
Allison Usher <allyjay317@gmail.com>
2021-02-01 19:24:12 -0800

    Create React App

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import RecipePage from "./components/RecipePage";
+
+function App() {
+  return (
+    <div className="App">
+      <RecipePage status="create" />
+    </div>
+  );
+}
+
+export default App;
