a495166454a1078388a205e42494b73dd814642f
Allison Usher <allyjay317@gmail.com>
2021-02-26 19:16:50 -0800

    Created User Recipe List

---
+++ src/components/User/UserRecipeList.js
@@ -0,0 +1,38 @@
+import {
+  Card,
+  CardActionArea,
+  CardContent,
+  CardMedia,
+  Grid,
+  Typography,
+} from "@material-ui/core";
+import React from "react";
+
+const UserRecipeList = ({ recipeList }) => {
+  return (
+    <Grid container xs={9} spacing={2} justify="center">
+      <Grid item xs={12}>
+        <Typography variant="h4">Your Recipes</Typography>
+      </Grid>
+
+      {recipeList.map((r) => (
+        <Grid item>
+          <Card xs={1} style={{ width: "200px" }}>
+            <CardActionArea>
+              <CardMedia
+                image={r.image}
+                title={r.name}
+                style={{ height: "200px" }}
+              />
+            </CardActionArea>
+            <CardContent>
+              <Typography variant="h5">{r.name}</Typography>
+            </CardContent>
+          </Card>
+        </Grid>
+      ))}
+    </Grid>
+  );
+};
+
+export default UserRecipeList;
