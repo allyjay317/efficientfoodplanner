7d19920360779529784a7c367cad4963504f9e17
Allison Usher <allyjay317@gmail.com>
2021-02-26 19:17:09 -0800

    added user components to user page

--- src/components/User/User.js
+++ src/components/User/User.js
@@ -1,7 +1,30 @@
+import {
+  Button,
+  Card,
+  CardActionArea,
+  CardContent,
+  CardHeader,
+  CardMedia,
+  Grid,
+  Typography,
+} from "@material-ui/core";
 import React from "react";
+import UserHeader from "./UserHeader";
+import UserMealPlan from "./UserMealPlan";
+import UserRecipeList from "./UserRecipeList";
+import UserShoppingList from "./UserShoppingList";
 
-const User = () => {
-  return <div></div>;
+const User = ({ user }) => {
+  return (
+    <Grid container>
+      <UserHeader name={user.name} />
+      <Grid container xs={3}>
+        <UserMealPlan mealPlan={user.mealPlan} />
+        <UserShoppingList shoppingList={user.shoppingList} />
+      </Grid>
+      <UserRecipeList recipeList={user.recipeList} />
+    </Grid>
+  );
 };
 
 export default User;
