382b1227e64e2b7c05cd9b47f9c1eb80665008b4
Allison Usher <allyjay317@gmail.com>
2021-02-26 19:40:27 -0800

    moved state management of user to reducer

--- src/App.js
+++ src/App.js
@@ -2,11 +2,21 @@ import "./App.css";
 import RecipePage from "./components/Recipe/RecipePage";
 import User from "./components/User/User";
 
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+
+import { default as reducer } from "./redux/Reducers";
+
+const store = createStore(reducer, applyMiddleware(thunk));
+
 function App() {
   return (
-    <div className="App">
-      <User user={defaultUser} />
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <User />
+      </div>
+    </Provider>
   );
 }
 
