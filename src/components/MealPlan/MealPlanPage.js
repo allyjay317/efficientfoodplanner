922e4d06bd58206ed8228db4e30235aaa74f4fda
Allison Usher <allyjay317@gmail.com>
2021-03-02 19:34:51 -0800

    added defaultMealPlan

--- src/components/MealPlan/MealPlanPage.js
+++ src/components/MealPlan/MealPlanPage.js
@@ -1,5 +1,36 @@
 import React from "react";
 
+const defaultMealPlan = [
+  {
+    name: "Orange Chicken",
+    ingredients: ["orange", "chicken", "soy sauce"],
+  },
+  {
+    name: "Orange Chicken",
+    ingredients: ["orange", "chicken", "soy sauce"],
+  },
+  {
+    name: "Orange Chicken",
+    ingredients: ["orange", "chicken", "soy sauce"],
+  },
+  {
+    name: "Orange Chicken",
+    ingredients: ["orange", "chicken", "soy sauce"],
+  },
+  {
+    name: "Orange Chicken",
+    ingredients: ["orange", "chicken", "soy sauce"],
+  },
+  {
+    name: "Orange Chicken",
+    ingredients: ["orange", "chicken", "soy sauce"],
+  },
+  {
+    name: "Orange Chicken",
+    ingredients: ["orange", "chicken", "soy sauce"],
+  },
+];
+
 const MealPlanPage = () => {
   return <div></div>;
 };
