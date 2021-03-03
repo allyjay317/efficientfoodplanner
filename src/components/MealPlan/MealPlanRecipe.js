1e560d67e823f261a6dc6165098d036f686f2974
Allison Usher <allyjay317@gmail.com>
2021-03-02 19:33:18 -0800

    Created MealPlanRecipe

---
+++ src/components/MealPlan/MealPlanRecipe.js
@@ -0,0 +1,17 @@
+import { TableCell, TableRow, Typography } from "@material-ui/core";
+import React from "react";
+
+const MealPlanRecipe = ({ meal }) => {
+  return (
+    <TableCell>
+      <Typography>{meal.name}</Typography>
+      <ul>
+        {meal.ingredients.map((i) => (
+          <li>{i}</li>
+        ))}
+      </ul>
+    </TableCell>
+  );
+};
+
+export default MealPlanRecipe;
