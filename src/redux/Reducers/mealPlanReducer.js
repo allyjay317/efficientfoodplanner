9dd70a50c1a98c8a0dd8cd7c42090c7d51e5cdea
Allison Usher <allyjay317@gmail.com>
2021-03-03 17:09:25 -0800

    moved defaultMealPlan to a reducer

---
+++ src/redux/Reducers/mealPlanReducer.js
@@ -0,0 +1,214 @@
+const defaultMealPlan = [
+  {
+    name: "Orange Chicken",
+    ingredients: [
+      {
+        id: 1,
+        name: "orange",
+        qty: 1,
+        unit: "Small",
+        pluralUnit: "Large",
+        pluralName: "Oranges",
+      },
+      {
+        id: 2,
+        name: "chicken",
+        qty: 200,
+        unit: "g",
+        pluralUnit: "lbs",
+        pluralName: "chicken",
+      },
+      {
+        id: 3,
+        name: "soy sauce",
+        qty: 1,
+        unit: "tbsp",
+        pluralUnit: "tbsps",
+        pluralName: "soy sauce",
+      },
+    ],
+  },
+  {
+    name: "Orange Chicken",
+    ingredients: [
+      {
+        id: 1,
+        name: "orange",
+        qty: 1,
+        unit: "Large",
+        pluralUnit: "Large",
+        pluralName: "Oranges",
+      },
+      {
+        id: 2,
+        name: "chicken",
+        qty: 1,
+        unit: "lb",
+        pluralUnit: "lbs",
+        pluralName: "chicken",
+      },
+      {
+        id: 3,
+        name: "soy sauce",
+        qty: 1,
+        unit: "tsp",
+        pluralUnit: "tbsps",
+        pluralName: "soy sauce",
+      },
+    ],
+  },
+  {
+    name: "Orange Chicken",
+    ingredients: [
+      {
+        id: 1,
+        name: "orange",
+        qty: 1,
+        unit: "Large",
+        pluralUnit: "Large",
+        pluralName: "Oranges",
+      },
+      {
+        id: 2,
+        name: "chicken",
+        qty: 1,
+        unit: "lb",
+        pluralUnit: "lbs",
+        pluralName: "chicken",
+      },
+      {
+        id: 3,
+        name: "soy sauce",
+        qty: 1,
+        unit: "tbsp",
+        pluralUnit: "tbsps",
+        pluralName: "soy sauce",
+      },
+    ],
+  },
+  {
+    name: "Orange Chicken",
+    ingredients: [
+      {
+        id: 1,
+        name: "orange",
+        qty: 1,
+        unit: "Large",
+        pluralUnit: "Large",
+        pluralName: "Oranges",
+      },
+      {
+        id: 2,
+        name: "chicken",
+        qty: 1,
+        unit: "lb",
+        pluralUnit: "lbs",
+        pluralName: "chicken",
+      },
+      {
+        id: 3,
+        name: "soy sauce",
+        qty: 1,
+        unit: "tbsp",
+        pluralUnit: "tbsps",
+        pluralName: "soy sauce",
+      },
+    ],
+  },
+  {
+    name: "Orange Chicken",
+    ingredients: [
+      {
+        id: 1,
+        name: "orange",
+        qty: 1,
+        unit: "Large",
+        pluralUnit: "Large",
+        pluralName: "Oranges",
+      },
+      {
+        id: 2,
+        name: "chicken",
+        qty: 1,
+        unit: "lb",
+        pluralUnit: "lbs",
+        pluralName: "chicken",
+      },
+      {
+        id: 3,
+        name: "soy sauce",
+        qty: 1,
+        unit: "tbsp",
+        pluralUnit: "tbsps",
+        pluralName: "soy sauce",
+      },
+    ],
+  },
+  {
+    name: "Orange Chicken",
+    ingredients: [
+      {
+        id: 1,
+        name: "orange",
+        qty: 1,
+        unit: "Large",
+        pluralUnit: "Large",
+        pluralName: "Oranges",
+      },
+      {
+        id: 2,
+        name: "chicken",
+        qty: 1,
+        unit: "lb",
+        pluralUnit: "lbs",
+        pluralName: "chicken",
+      },
+      {
+        id: 3,
+        name: "soy sauce",
+        qty: 1,
+        unit: "tbsp",
+        pluralUnit: "tbsps",
+        pluralName: "soy sauce",
+      },
+    ],
+  },
+  {
+    name: "Orange Chicken",
+    ingredients: [
+      {
+        id: 1,
+        name: "orange",
+        qty: 1,
+        unit: "Large",
+        pluralUnit: "Large",
+        pluralName: "Oranges",
+      },
+      {
+        id: 2,
+        name: "chicken",
+        qty: 1,
+        unit: "lb",
+        pluralUnit: "lbs",
+        pluralName: "chicken",
+      },
+      {
+        id: 3,
+        name: "soy sauce",
+        qty: 1,
+        unit: "tbsp",
+        pluralUnit: "tbsps",
+        pluralName: "soy sauce",
+      },
+    ],
+  },
+];
+
+const mealPlanReducer = (state = defaultMealPlan, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default mealPlanReducer;
